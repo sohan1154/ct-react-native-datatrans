37b0ca528c235408545db9c46c53474b095ac85b
sabash-c <sabash.c@blaze.ws>
2021-06-03 18:05:50 +0530

    fixed datatrans intagration

--- android/src/main/java/com/reactnativedatatrans/DatatransModule.java
+++ android/src/main/java/com/reactnativedatatrans/DatatransModule.java
@@ -21,12 +21,13 @@ import ch.datatrans.payment.api.Transaction;
 import ch.datatrans.payment.api.TransactionListener;
 import ch.datatrans.payment.api.TransactionRegistry;
 import ch.datatrans.payment.api.TransactionSuccess;
+import ch.datatrans.payment.exception.TechnicalException;
 import ch.datatrans.payment.exception.TransactionException;
 
 @ReactModule(name = DatatransModule.NAME)
 public class DatatransModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "Datatrans";
-    private Context mActivityContext;
+  public static final String NAME = "Datatrans";
+  private Context mActivityContext;
   private final String CALLBACK_TYPE_SUCCESS = "success";
   private final String CALLBACK_TYPE_ERROR = "error";
   private final String CALLBACK_TYPE_CANCEL = "cancel";
@@ -35,73 +36,75 @@ public class DatatransModule extends ReactContextBaseJavaModule {
   private JSONObject jsonobject;
 
 
-
-
   public DatatransModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-     // mActivityContext = activityContext;
+    super(reactContext);
+    // mActivityContext = activityContext;
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void transaction(@NonNull String mobileToken, String aliasPaymentMethods, Promise promise) {
+    ReactApplicationContext context = getReactApplicationContext();
+    Activity activity = getCurrentActivity();
+    if (activity == null) {
+      promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
+      return;
     }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
+    if (activity != null) {
+      Intent intent = new Intent(context, ExternalProcessRelayActivity.class);
+      activity.startActivity(intent);
 
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void transaction(@NonNull String mobileToken, String aliasPaymentMethods, Promise promise) {
-      ReactApplicationContext context = getReactApplicationContext();
-      Activity activity = getCurrentActivity();
-      if (activity == null) {
-        promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
-        return;
-      }
-
-      if (activity != null) {
-        Intent intent = new Intent(context, ExternalProcessRelayActivity.class);
-        activity.startActivity(intent);
-
+      try {
 
 
         Transaction transaction = new Transaction(mobileToken);
-transaction.setListener(new TransactionListener() {
-  @Override
-  public void onTransactionSuccess(TransactionSuccess transactionSuccess) {
-    Toast.makeText(getReactApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
-    promise.resolve(transactionSuccess);
-  }
+        transaction.setListener(new TransactionListener() {
+          @Override
+          public void onTransactionSuccess(TransactionSuccess transactionSuccess) {
+            Toast.makeText(getReactApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+            promise.resolve(transactionSuccess);
+          }
 
-  @Override
-  public void onTransactionError(TransactionException e) {
-    Toast.makeText(getReactApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-    promise.resolve(e);
-  }
+          @Override
+          public void onTransactionError(TransactionException e) {
+            Toast.makeText(getReactApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            promise.resolve(e);
+          }
 
-  @Override
-  public void onTransactionCancel() {
-    Toast.makeText(getReactApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
-    promise.reject("Error");
-  }
-}); // this refers to Android's Activity
-transaction.getOptions().setAppCallbackScheme("your_scheme");
-transaction.getOptions().setTesting(true);
-transaction.getOptions().setUseCertificatePinning(true);
-TransactionRegistry.INSTANCE.startTransaction(activity, transaction);
+          @Override
+          public void onTransactionCancel() {
+            Toast.makeText(getReactApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
 
+          }
+        }); // this refers to Android's Activity
+        transaction.getOptions().setAppCallbackScheme("your_scheme");
+        transaction.getOptions().setTesting(true);
+        transaction.getOptions().setUseCertificatePinning(true);
+        TransactionRegistry.INSTANCE.startTransaction(activity, transaction);
+        throw  new TechnicalException("Technical Error",null,null,null);
+        //promise.resolve(transaction);
+      } catch (TechnicalException e) {
 
-
+        promise.reject(e);
       }
     }
-  @ReactMethod
-  public void multiply(int a, int b, Promise promise) {
-    promise.resolve(a*b);
   }
 
-    public static native int nativeMultiply(int a, int b);
+  @ReactMethod
+  public void multiply(int a, int b, Promise promise) {
+    promise.resolve(a * b);
+  }
 
+  public static native int nativeMultiply(int a, int b);
 
 
 }

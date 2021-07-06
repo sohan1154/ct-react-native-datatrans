ab3d57090cc98717207305b4a806132e32efc907
sabash-c <sabash.c@blaze.ws>
2021-07-06 13:16:49 +0530

    android code updated

--- android/src/main/java/com/reactnativedatatrans/DatatransModule.java
+++ android/src/main/java/com/reactnativedatatrans/DatatransModule.java
@@ -7,14 +7,24 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import ch.datatrans.payment.ExternalProcessRelayActivity;
 import ch.datatrans.payment.api.Transaction;
@@ -23,6 +33,9 @@ import ch.datatrans.payment.api.TransactionRegistry;
 import ch.datatrans.payment.api.TransactionSuccess;
 import ch.datatrans.payment.exception.TechnicalException;
 import ch.datatrans.payment.exception.TransactionException;
+import ch.datatrans.payment.paymentmethods.CardToken;
+import ch.datatrans.payment.paymentmethods.PaymentMethodToken;
+import ch.datatrans.payment.paymentmethods.PaymentMethodType;
 
 @ReactModule(name = DatatransModule.NAME)
 public class DatatransModule extends ReactContextBaseJavaModule {
@@ -34,7 +47,9 @@ public class DatatransModule extends ReactContextBaseJavaModule {
   private static final String E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST";
   private JSONArray jsonarray;
   private JSONObject jsonobject;
-
+  private WritableMap mapObj;
+  //private Callback mTokenCallback;
+  private  Promise mPromise;
 
   public DatatransModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -51,53 +66,115 @@ public class DatatransModule extends ReactContextBaseJavaModule {
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  public void transaction(@NonNull String mobileToken, String aliasPaymentMethods, Promise promise) {
+  public void transaction(String mobileToken, ReadableMap options, final Promise promise) {
     ReactApplicationContext context = getReactApplicationContext();
     Activity activity = getCurrentActivity();
+    mPromise = promise;
     if (activity == null) {
-      promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
+      WritableMap map = Arguments.createMap();
+      map.putString("message", "Activity doesn't exist");
+      consumeCallback(E_ACTIVITY_DOES_NOT_EXIST, map);
       return;
     }
 
+    //aliasPaymentMethods
     if (activity != null) {
       Intent intent = new Intent(context, ExternalProcessRelayActivity.class);
       activity.startActivity(intent);
-
+      Transaction transaction;
       try {
+        Collection paymentCollection= new ArrayList<>();
+        //PaymentMethodType
+        //PaymentMethodToken pmt;
+       // CardToken ct =new CardToken();
+      //  ct.setToken("SDFASDF");
+      //  ct.
+        //paymentCollection.add(aliasPaymentMethods);
+        List<PaymentMethodToken> paymentMethodTokens = new ArrayList<>();
+        paymentMethodTokens.addAll(paymentCollection);
 
+      //modules.add(new DatatransModule(reactContext));
 
-        Transaction transaction = new Transaction(mobileToken);
-        transaction.setListener(new TransactionListener() {
-          @Override
-          public void onTransactionSuccess(TransactionSuccess transactionSuccess) {
-            Toast.makeText(getReactApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
-            promise.resolve(transactionSuccess);
-          }
+        if(paymentMethodTokens.size()>0) {
+          transaction = new Transaction(mobileToken, paymentMethodTokens);
+        }
+        else{
+          transaction = new Transaction(mobileToken);
+        }
 
-          @Override
-          public void onTransactionError(TransactionException e) {
-            Toast.makeText(getReactApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-            promise.resolve(e);
-          }
+                // transaction = new Transaction(mobileToken);
+      TransactionListener transactionListener= new TransactionListener() {
+        @Override
+        public void onTransactionCancel() {
+          WritableMap map = Arguments.createMap();
+          WritableMap data = Arguments.createMap();
 
-          @Override
-          public void onTransactionCancel() {
-            Toast.makeText(getReactApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+          map.putMap("Data",data);
+          map.putString("action","Cancel");
 
-          }
-        }); // this refers to Android's Activity
-        transaction.getOptions().setAppCallbackScheme("your_scheme");
-        transaction.getOptions().setTesting(true);
-        transaction.getOptions().setUseCertificatePinning(true);
+          Toast.makeText(getReactApplicationContext(), "Cancel", Toast.LENGTH_LONG).show();
+          mapObj=map;
+          consumeCallback(CALLBACK_TYPE_CANCEL, map);
+        }
+
+        @Override
+        public void onTransactionError(@NotNull TransactionException e) {
+          WritableMap map = Arguments.createMap();
+          WritableMap data = Arguments.createMap();
+          data.putString("transactionId", e.getTransactionId());
+          data.putString("message", e.getMessage());
+          data.putString("paymentMethodType", e.getPaymentMethodType().getIdentifier());
+          map.putMap("Data",data);
+          map.putString("action","Error");
+          Toast.makeText(getReactApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+          mapObj=map;
+          consumeCallback(CALLBACK_TYPE_ERROR, map);
+        }
+
+        @Override
+        public void onTransactionSuccess(@NotNull TransactionSuccess transactionSuccess) {
+          WritableMap map = Arguments.createMap();
+          WritableMap data = Arguments.createMap();
+          data.putString("transactionId", transactionSuccess.getTransactionId());
+          data.putString("paymentMethodToken", transactionSuccess.getPaymentMethodToken().toString());
+          data.putString("paymentMethodType", transactionSuccess.getPaymentMethodType().getIdentifier());
+
+          map.putMap("Data",data);
+          map.putString("action","Finish");
+
+          Toast.makeText(getReactApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+          mapObj=map;
+          consumeCallback(CALLBACK_TYPE_SUCCESS, map);
+        }
+      };
+      transaction.setListener(transactionListener); // this refers to Android's Activity
+        transaction.getOptions().setAppCallbackScheme(options.getString("appCallbackScheme"));
+        transaction.getOptions().setTesting(options.getBoolean("isTesing") );
+        transaction.getOptions().setUseCertificatePinning(options.getBoolean("isUseCertificatePinning"));
         TransactionRegistry.INSTANCE.startTransaction(activity, transaction);
-        throw  new TechnicalException("Technical Error",null,null,null);
-        //promise.resolve(transaction);
-      } catch (TechnicalException e) {
+
+     // consumeCallback("temp", mapObj);
+      //promise.resolve(callback);
+        //throw  new TechnicalException("Technical Error",null,null,null);
+        //promise.resolve(transaction.getListener().toString());
+      } catch (Exception e) {
 
         promise.reject(e);
       }
     }
+
   }
+
+  private void consumeCallback(String type, WritableMap map) {
+    if (mPromise != null) {
+      map.putString("type", type);
+      map.putString("provider", "ct-datatrans");
+
+      mPromise.resolve(map);
+      mPromise = null;
+    }
+  }
+
 
   @ReactMethod
   public void multiply(int a, int b, Promise promise) {
